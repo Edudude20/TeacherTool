b81e36552a0df287d24951e1fe00009d5aebe61e
EetuAMK <eetu.nummelin@edu.turkuamk.fi>
2023-09-25 15:12:03 +0300

    scaffold structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <header>
+        <h1>AIIS CLP (Collaborative learning platform)</h1>
+        <button>Toggle languages (dropdown)</button>
+      </header>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h2>1. Title</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Please write the title of the task. This will show for the students as
+          they select their task from the task machine so make it clear.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h2>2. Slide</h2>
+        <p>
+          Please write the description of the task for the students. These will
+          show as slide (similarly to PowerPoint) for the students as they open
+          the task.
+        </p>
+      </div>
+      <div>
+        <h2>3. Learning material (optional)</h2>
+        <p>
+          You can add a video (GIF) explaining for example the task type (e.g.
+          match-the columns) or the subject of the module. Video must be under
+          XXMb in size.
+        </p>
+      </div>
+      <div>
+        <h2>4. Task settings</h2>
+        <p>
+          This is the minigame that the students will play to finish the task.
+        </p>
+        <p>Please select a task type using the dropdown menu below.</p>
+        <button>Select task type (dropdown)</button>
+        <div>
+          <p>
+            Task description: drag-and-drop the answer options to the open slots
+            of their respective matches. Leave column entry (box on the right
+            side) empty if this is a false match. Options are draggable.
+          </p>
+          <p>Options x/10</p>
+          <ol>
+            <li>Option 1</li>
+            <li>Option 2</li>
+          </ol>
+          <button>
+            add option +
+          </button>
+          <p>Options x/10</p>
+        </div>
+      </div>
+      <button>Submit/Continue/Preview</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
